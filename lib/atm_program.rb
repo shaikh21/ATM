c47875eb326276216a2f654cf4b3804671c1d4f8
Zaid Shaikh <41386454+shaikh21@users.noreply.github.com>
2022-03-23 14:11:18 +0530

    Add files via upload

---
+++ lib/atm_program.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require 'base64'
+require_relative 'atm'
+require 'highline/import'
+require 'colorize'
+
+module ATM
+  def self.start
+    Atm.new
+  end
+end
+
+ATM.start
